60770fadc91c5b514d5d2d80b1e76835a32baf04
Davey1-2 <david.barta2014gmail.com>
2021-03-18 11:32:34 +0100

    Final, works now

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,15 +16,12 @@ public class Main {
             String line = br.readLine();
             int countLine = 0;
             boolean exit = true;
-            boolean allExit = true;
 
-            while (allExit = true) {
-                exit = true;
+
+
                 System.out.println("Zadej jmeno: ");
                 jmeno = sc.nextLine();
-                if (jmeno.equals("end1")){
-                    allExit = false;
-                }
+
                 if (countLine == 0) {
                     System.out.println("-- žádné předchozí zprávy--");
                 } else {
@@ -35,13 +32,10 @@ public class Main {
                     }
                     System.out.println("-------------------------------------------");
                 }
-                System.out.println("Pro ukončení zápisu zpráv zadejte `konec1`, pro uplny konec zadej `end1`");
+                System.out.println("Pro ukončení zápisu zpráv zadejte `konec1`");
                 while (exit) {
                     zprava = sc.nextLine();
-                    if (zprava.equals("end")){
-                        allExit = false;
-                        System.exit(0);
-                    }
+
                     if (zprava.equals("konec1")) {
                         exit = false;
                     } else {
@@ -51,7 +45,7 @@ public class Main {
                     }
                 }
                 bw.close();
-            }
+
 
 
         } catch (FileNotFoundException e) {
